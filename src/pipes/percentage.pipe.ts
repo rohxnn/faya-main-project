d0cc3769c7c591da2f9b3a298e9f3bebed43b605
Rohan Thomas Reji <rohanreji123@gmail.com>
2023-12-20 08:56:30 +0530

    fix(input-output-decorator): update changes

--- src/pipes/percentage.pipe.ts
+++ src/pipes/percentage.pipe.ts
@@ -4,8 +4,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'percentage'
 })
 export class PercentagePipe implements PipeTransform {
-  transform(value: any, total_mark: number) {
+  transform(value: any, total_mark: number,decimal:number) {
     const result = (value / total_mark) * 100;
-    return result.toFixed(2);
+    return result.toFixed(decimal);
   }
 }
